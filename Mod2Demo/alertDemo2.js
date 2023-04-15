c6ee7253977a85dda6d80a459126e60121143605
AaronweiseRasmussen <130181118+AaronweiseRasmussen@users.noreply.github.com>
2023-04-14 21:48:26 -0500

    Add files via upload

---
+++ Mod2Demo/alertDemo2.js
@@ -0,0 +1,6 @@
+// Alert function to gather user name using alert() function. 
+
+// Use window.prompt() to get an input.
+var username = window.prompt("Enter Name Please");
+// Use alert() to display the name.
+alert(`Greetings, ${username}!`);
---
+++ Mod2Demo/alertDemo3.js
@@ -0,0 +1,24 @@
+// Alert function to gather user name using alert() function. 
+
+// Use window.prompt() to get an input.
+var username = window.prompt("Enter Name Please");
+// Use alert() to display the name.
+alert(`Greetings, ${username}!`);
+
+
+var myFavNum = window.prompt("Enter your Favorite Number:");
+
+//stores a value that wont change.
+const piValue = 3.1415926;
+
+var myArea = parseInt(piValue) * parseInt(myFavNum) * parseInt(myFavNum);
+
+
+
+
+
+alert(`Hello, ${username}! You entered ${myFavNum} as your FAVORITE number. 
+Using that as the radius of a circle,
+you would get ${myArea} as the Area.`);
+
+
